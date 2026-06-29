c1a8a925f06cb3236ac2a48245f7c01859a404e1
zhangtao <cyankate@163.com>
2026-06-29 18:33:42 +0800

    更新数据

--- script/setting/RECOVERY_DATA.lua
+++ script/setting/RECOVERY_DATA.lua
@@ -1,5 +1,5 @@
 return 
 {
-[1]={["Id"]=1,["Enum"]="STAMINA",["Type"]=3,["InitCount"]=100,["MaxLimit"]=200,["RecoverLimit"]=100,["RecoverCount"]=1,["RecoverDuration"]=5},
+[1]={["Id"]=1,["Enum"]="STAMINA",["Type"]=3,["InitCount"]=100,["MaxLimit"]=200,["RecoverLimit"]=100,["RecoverCount"]=1,["RecoverDuration"]=1},
 [2]={["Id"]=2,["Enum"]="FIGHT_TIMES",["Type"]=1,["InitCount"]=100,["MaxLimit"]=10,["RecoverDuration"]=5},
 [3]={["Id"]=3,["Enum"]="REWARD_DOUBLES",["Type"]=2,["InitCount"]=100,["MaxLimit"]=10,["RecoverDuration"]=5}}
