00fbdb1cf74bdf6e485d3ba536312b1c791b9d4d
zhangtao <cyankate@163.com>
2026-07-01 18:31:47 +0800

    更新数据

--- script/setting/INSTANCE_DATA.lua
+++ script/setting/INSTANCE_DATA.lua
@@ -1,3 +1,3 @@
 return 
 {
-[1]={["Id"]=1,["Name"]="沙漠废土",["PassReward"]={[802]=100},["SelectNeedEnergy"]={[1]=4,[2]=5,[3]=6},["RogueRefreshId"]=1}}
+[1]={["Id"]=1,["Name"]="沙漠废土",["PassReward"]={[802]=100},["SelectNeedEnergy"]={[1]=4,[2]=5,[3]=6,[4]=7,[5]=8},["RogueRefreshId"]=1}}
